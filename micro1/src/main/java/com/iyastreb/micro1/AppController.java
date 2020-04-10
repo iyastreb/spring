cb705156aa4281ddaa5942354b3535cd8fc81534
Ilia Yastrebov <iyastreb@cisco.com>
2020-04-10 18:27:35 +0200

    integrated with config

--- micro1/src/main/java/com/iyastreb/micro1/AppController.java
+++ micro1/src/main/java/com/iyastreb/micro1/AppController.java
@@ -1,6 +1,7 @@
 package com.iyastreb.micro1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +15,15 @@ public class AppController {
 	@Autowired
 	private TeamDao teamDao;
 
+	@Value("${name}") String name;
+
 	@RequestMapping("/old/{name}")
 	public @ResponseBody Team team(@PathVariable String name) {
 		return teamDao.findByName(name);
+	}
+	
+	@RequestMapping("/name")
+	public @ResponseBody String name() {
+		return "My name is " + name;
 	}
 }
